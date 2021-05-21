7235868647cd233b63123cc33d0ad0b0567ab989
crocokyle <me@kyle-dixon.com>
2021-05-21 08:00:58 +0000

    Started processing loop for text data into report format

--- report_email.py
+++ report_email.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+import reports
+import os
+import datetime
+
+def __main__():
+  desc_dir = "supplier-data/descriptions"
+  report_path = "/tmp/processed.pdf"
+  report_title = "Processed on {}".format(datetime.date.today())
+
+  item_collector = {}
+  for file in os.listdir(desc_dir):
+    if file[-4:] == ".txt":
+      with open(os.path.join(desc_dir, file), 'r') as f:
+        line_collector = []
+        for line in f.readlines():
+          line_collector.append(line[:-1])
+        item_collector['name'] = line_collector[0]
+        item_collector['weight'] = line_collector[1]
+        
