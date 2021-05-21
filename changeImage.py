ad9cb7ac8312ee7a6ec70f85194a3aa4ba842d67
crocokyle <me@kyle-dixon.com>
2021-05-21 06:43:37 +0000

    Adding loop and image loading

--- changeImage.py
+++ changeImage.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+import os
+import tqdm
+from PIL import Image
+
+image_path = "~/supplier-data/images"
+
+for pic in tqdm(os.listdir(image_path)):
+   img = Image.open(os.path.join(image_path, pic)).convert("RGB").resize((600, 400))
