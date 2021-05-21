3f1aea1e0e68653904551e94cbe5ef7a4ba46a34
crocokyle <me@kyle-dixon.com>
2021-05-21 09:55:57 +0000

    Misc syntax errors and backwards conditionals

--- emails.py
+++ emails.py
@@ -12,11 +12,11 @@ def generate_email(sender, recipient, subject, body, attachment_path):
   message["Subject"] = subject
   message.set_content(body)
 
-  attachment_filename = os.path.basename(attachment_path)
-  mime_type, _ = mimetypes.guess_type(attachment_path)
-  mime_type, mime_subtype = mime_type.split('/', 1)
-
   if attachment_path:
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
+
     with open(attachment_path, 'rb') as ap:
       message.add_attachment(ap.read(), maintype=mime_type, subtype=mime_subtype, filename=attachment_filename)
 
--- health_check.py
+++ health_check.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python3
 
+import emails
 import shutil
 import psutil
 import time
+import socket
 import os
 
 def email_status(subject):
   sender = "automation@example.com"
   receiver = "{}@example.com".format(os.environ["USER"])
   body = "Please check your system and resolve the issue as soon as possible."
-  message = emails.generate_email(sender, receiver, subject, body)
-  emails.server_email(message)
+  message = emails.generate_email(sender, receiver, subject, body, False)
+  emails.send_email(message)
 
 def main():
   cpu_usage = psutil.cpu_percent(1)
-  cpu_status = spu_usage > 80
+  cpu_status = cpu_usage > 80
+  print(cpu_status)
 
   disk_usage = shutil.disk_usage("/").used / shutil.disk_usage("/").total * 100
-  disk_status = disk_usage > 20
+  disk_status = disk_usage < 20
 
   memory_usage = psutil.virtual_memory().available / 1024**2
-  memory_status = memory_usage > 500
+  memory_status = memory_usage < 500
 
-  network_status = socket.gethostbyname('localhost') == "127.0.0.1"
+  network_status = socket.gethostbyname('localhost') != "127.0.0.1"
 
-  if not cpu_status:
+  if cpu_status:
+    print("Error - CPU usage is over 80%")
     email_status("Error - CPU usage is over 80%")
 
-  if not disk_status:
+  if disk_status:
+    print("Error - Available disk space is less than 20%")
     email_status("Error - Available disk space is less than 20%")
 
-  if not memory_usage:
+  if memory_usage:
+    print("Error - Available memory is less than 500MB")
     email_status("Error - Available memory is less than 500MB")
 
-  if not network_status:
+  if network_status:
+    print("Error - localhost cannot be resolved to 127.0.0.1")
     email_status("Error - localhost cannot be resolved to 127.0.0.1")
+
+if __name__ == "__main__":
+  main()
