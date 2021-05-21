ddf822b2fe4139ef73eb4303d7c89768e1d5427d
crocokyle <me@kyle-dixon.com>
2021-05-21 07:27:53 +0000

    Adding requests JSON parser and upload loop

--- run.py
+++ run.py
@@ -3,5 +3,24 @@
 import os
 import requests
 
+txt_path = "supplier-data/descriptions"
+url = "http://localhost/fruits"
 
 
+for txt in os.listdir(txt_path):
+  if txt[-4:] = ".txt":
+    line_list = []
+    dictionary = {}
+    with open(os.path.join(txt_path, txt), 'r') as file:
+      for line in file.readlines():
+        line_list.append(line[:-1])
+ 
+      dictionary['name'] = line_list[0]
+      dictionary['weight'] = int(line_list[1][:-4])
+      dictionary['description'] = line_list[2]
+
+    print(dictionary)
+    x = requests.post(url, json=dictionary)
+    if not x.ok:
+      print(x.status_code)
+      print(x.text)
