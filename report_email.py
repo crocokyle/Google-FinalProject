73c2c9859d0b1ff09b5c9e140bdde4935eb72fea
crocokyle <me@kyle-dixon.com>
2021-05-21 08:10:29 +0000

    Improved readlines performance

--- report_email.py
+++ report_email.py
@@ -4,14 +4,19 @@ import reports
 import os
 import datetime
 
-def __main__():
+def parseData():
   desc_dir = "supplier-data/descriptions"
   report_path = "/tmp/processed.pdf"
   report_title = "Processed on {}".format(datetime.date.today())
 
-  item_collector = {}
+  item_collector = []
   for file in os.listdir(desc_dir):
     if file[-4:] == ".txt":
       with open(os.path.join(desc_dir, file), 'r') as f:
         content = f.readlines()
-        item_collector.append("name: {}\nweight: {}\n\n".format(content[0], content[1]))
+        item_collector.append("name: {}weight: {}\n".format(content[0], content[1]))
+
+  print(item_collector)
+
+if __name__ == "__main__":
+  parseData()
