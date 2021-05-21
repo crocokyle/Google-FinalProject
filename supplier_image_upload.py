6e34bc2d781aa418078fb93b223994be8cc59943
crocokyle <me@kyle-dixon.com>
2021-05-21 07:11:32 +0000

    Writing upload loop

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import requests
+import os
+
+url = "http://localhost/upload/"
+image_path = "supplier-data/images"
+
+for pic in os.listdir(image_path):
+  if pic[-4:] == "jpeg":
+    with open(os.path.join(image_path, pic), 'rb') as file:
+      r = requests.psot(url, files={'file': file})
+      if not r.ok:
+        print(r.status)
+        print(r.text)
+
