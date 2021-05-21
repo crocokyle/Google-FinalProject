d9170c6fdd8a6fbdaf3dae37ef0d8acff73873cf
crocokyle <me@kyle-dixon.com>
2021-05-21 07:01:08 +0000

    Corrected output file paths and conditional

--- changeImage.py
+++ changeImage.py
@@ -7,6 +7,6 @@ from PIL import Image
 image_path = "supplier-data/images"
 
 for pic in tqdm(os.listdir(image_path)):
-   if pic[-4:] == ".tiff":
-     f_name = pic[:-4] + "jpg"
-     img = Image.open(os.path.join(image_path, pic)).convert("RGB").resize((600, 400)).save(os.path.join(image_path, f_name), "JPEG", quality=100)
+  if str(pic[-4:]) == "tiff":
+    f_name = pic[:-4] + "jpg"
+    img = Image.open(os.path.join(image_path, pic)).convert("RGB").resize((600, 400)).save(os.path.join(image_path, f_name), "JPEG", quality=100)
