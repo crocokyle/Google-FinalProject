67f87625b4cd1f3f7a89b18e144a66065c347c82
crocokyle <me@kyle-dixon.com>
2021-05-21 06:47:44 +0000

    Adding output filenames and .save()

--- changeImage.py
+++ changeImage.py
@@ -7,4 +7,5 @@ from PIL import Image
 image_path = "~/supplier-data/images"
 
 for pic in tqdm(os.listdir(image_path)):
-   img = Image.open(os.path.join(image_path, pic)).convert("RGB").resize((600, 400))
+   f_name = pic[:-4] + ".jpg"
+   img = Image.open(os.path.join(image_path, pic)).convert("RGB").resize((600, 400)).save(os.path.join(image_path, f_name), "JPEG", quality=100)
